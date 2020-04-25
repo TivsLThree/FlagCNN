0be880e6046284471b5a7d3f9eb85dbc70d468d9
Levi Huchingson <levihuchingson@gmail.com>
2020-04-25 15:46:44 -0400

    Updates

---
+++ precache-manifest.09206038fd2380ab280ea79d850c19e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "636f9ce0e2245b3ccbc761e7cb48fdbf",
+    "url": "/FlagCNN/index.html"
+  },
+  {
+    "revision": "37334459cb1aabe7d342",
+    "url": "/FlagCNN/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "47df6210ff9c5730e29d",
+    "url": "/FlagCNN/static/js/2.9fe3b46c.chunk.js"
+  },
+  {
+    "revision": "203136b256d73b86d9b5f7bd77fb7e0d",
+    "url": "/FlagCNN/static/js/2.9fe3b46c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37334459cb1aabe7d342",
+    "url": "/FlagCNN/static/js/main.569fa5eb.chunk.js"
+  },
+  {
+    "revision": "7c2e61c559a4b9df9d20",
+    "url": "/FlagCNN/static/js/runtime-main.e56dae21.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/FlagCNN/static/media/logo.5d5d9eef.svg"
+  }
+]);
